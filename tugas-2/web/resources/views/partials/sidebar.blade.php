50f7f7e6449d85f1599190b0957922d490c99224
McShinyShoe <mcshinyshoe@gmail.com>
2024-09-08 17:40:08 +0700

    feat: added some pages and routes

---
+++ tugas-2/web/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,45 @@
+<!-- Sidebar -->
+<div class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" style="box-shadow: 2px 0 10px rgba(0, 0, 0, 0.1);">
+    <div class="position-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/home">
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    View
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li><a class="dropdown-item" href="/home/view/faculty">Faculty</a></li>
+                    <li><a class="dropdown-item" href="/home/view/department">Department</a></li>
+                    <li><a class="dropdown-item" href="/home/view/student">Student</a></li>
+                    <li><a class="dropdown-item" href="/home/view/lecturer">Lecturer</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Create
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li><a class="dropdown-item" href="/home/create/faculty">Faculty</a></li>
+                    <li><a class="dropdown-item" href="/home/create/department">Department</a></li>
+                    <li><a class="dropdown-item" href="/home/create/student">Student</a></li>
+                    <li><a class="dropdown-item" href="/home/create/lecturer">Lecturer</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Edit
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li><a class="dropdown-item" href="/home/edit/faculty">Faculty</a></li>
+                    <li><a class="dropdown-item" href="/home/edit/department">Department</a></li>
+                    <li><a class="dropdown-item" href="/home/edit/student">Student</a></li>
+                    <li><a class="dropdown-item" href="/home/edit/lecturer">Lecturer</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
--- tugas-2/web/routes/web.php
+++ tugas-2/web/routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvenOddController;
+use App\Http\Controllers\FibonacciController;
+use App\Http\Controllers\PrimeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
+});
+
+Route::fallback(function () {
+    return response()->view('template.err.404', [], 404);
+});
+
+Route::prefix('/home')->group(function() {
+    Route::get('/', function () {
+        return view('template.home');
+    });
+    Route::get('/view/{category}', function () {
+        return view('template.view');
+    });
+    Route::get('/create/{category}', function () {
+        return view('template.create');
+    });
+    Route::get('/edit/{category}', function () {
+        return view('template.edit');
+    });
+});
+
+Route::get('/about', function () {
+    return view('template.about');
 });
