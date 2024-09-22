e47ed8ad928d15c2540fed1d742fc46dedbab3ca
McShinyShoe <mcshinyshoe@gmail.com>
2024-09-22 18:35:27 +0700

    feat: changed page opened when clicking PBKK to home page

--- tugas-3/web/resources/views/partials/navbar.blade.php
+++ tugas-3/web/resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/dashboard">PBKK</a>
+        <a class="navbar-brand" href="/home">PBKK</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
